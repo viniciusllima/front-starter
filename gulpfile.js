f85864d3c2b4010d44cf5336cf028911ed41479f
Vinicius Lima <viniciusllima@gmail.com>
2016-12-11 10:06:35 -0200

    First package release (1.0).

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var sass         = require('gulp-sass');
+var browserSync  = require('browser-sync');
+var uglify       = require('gulp-uglify');
+var plumber      = require('gulp-plumber');
+var rename       = require("gulp-rename");
+var imagemin     = require("gulp-imagemin");
+var pngquant     = require('imagemin-pngquant');
+
+gulp.task('browser-sync', function() {
+  browserSync.init(['src/assets/css/*.css', 'src/assets/js/**/*.js', 'index.html'], {
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('scripts', function() {
+  gulp.src('src/assets/js/*.js')
+  .pipe(uglify())
+  .pipe(rename({
+    dirname: "min",
+    suffix: ".min",
+  }))
+  .pipe(gulp.dest('js'))
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/assets/sass/**/*.scss')
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(plumber())
+  .pipe(gulp.dest('css'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('src/assets/images/*')
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest('images'));
+});
+
+gulp.task('default', ['browser-sync', 'sass', 'scripts'], function() {
+  gulp.watch('src/assets/sass/**/*.scss', ['sass']);
+  gulp.watch('src/assets/js/**/*.js', ['scripts']);
+});
---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+(function (window) {
+  'use strict'
+
+  // -- Vanilla JS
+
+
+}(this));
